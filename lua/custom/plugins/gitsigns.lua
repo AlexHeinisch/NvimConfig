21393ecd90f63d4236125b9c561814ad45754d31
Alexander Heinisch <alexander.heinisch@rise-world.com>
2025-12-20 14:37:52 +0100

    Add gitblame bar

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,13 +1,20 @@
 -- See `:help gitsigns` to understand what the configuration keys do
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
     },
+  },
+  keys = {
+    -- Toggle inline blame (virtual text at end of line)
+    { '<leader>hb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Toggle Blame Line' },
+
+    -- Or open full blame window (fugitive-style)
+    { '<leader>hB', '<cmd>Gitsigns blame<cr>', desc = 'Blame Buffer' },
+  }
 }
