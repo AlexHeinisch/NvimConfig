32d6ec495667ab060ee8d4080d5f99af0cb0bf2a
Alexander Heinisch <alexander.heinisch@rise-world.com>
2026-04-29 08:51:00 +0200

    Revert huge formatting config

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -3,70 +3,58 @@ return { -- Autoformat
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
     keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
+        {
+            '<leader>f',
+            function()
+                require('conform').format { async = true, lsp_format = 'fallback' }
+            end,
+            mode = '',
+            desc = '[F]ormat buffer',
+        },
     },
     opts = {
-      notify_on_error = false,
-      -- Respect buffer-local settings
-      format_on_save = function(bufnr)
-        -- Disable autoformat for certain filetypes
-        local disable_filetypes = { c = true, cpp = true, java = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil -- disable auto-format for these filetypes
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = 'fallback',
-        }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        java = { lsp_format = 'prefer' },
-        rust = { 'rustfmt' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-      -- Configure formatters to respect buffer settings
-      formatters = {
-        stylua = {
-          prepend_args = function(self, ctx)
-            -- Respect buffer indentation settings detected by guess-indent
-            local indent_type = vim.bo[ctx.buf].expandtab and 'Spaces' or 'Tabs'
-            local indent_width = vim.bo[ctx.buf].shiftwidth
-            return {
-              '--indent-type',
-              indent_type,
-              '--indent-width',
-              tostring(indent_width),
-            }
-          end,
+        notify_on_error = false,
+        formatters_by_ft = {
+            lua = { 'stylua' },
+            java = { lsp_format = 'prefer' },
+            rust = { 'rustfmt' },
+            -- Conform can also run multiple formatters sequentially
+            -- python = { "isort", "black" },
+            --
+            -- You can use 'stop_after_first' to run the first available formatter from the list
+            -- javascript = { "prettierd", "prettier", stop_after_first = true },
         },
-        rustfmt = {
-          prepend_args = function(self, ctx)
-            -- Respect buffer indentation settings detected by guess-indent
-            local args = {}
-            if vim.bo[ctx.buf].expandtab then
-              table.insert(args, '--config')
-              table.insert(args, 'hard_tabs=false')
-              table.insert(args, '--config')
-              table.insert(args, 'tab_spaces=' .. tostring(vim.bo[ctx.buf].shiftwidth))
-            else
-              table.insert(args, '--config')
-              table.insert(args, 'hard_tabs=true')
-            end
-            return args
-          end,
+        -- Configure formatters to respect buffer settings
+        formatters = {
+            stylua = {
+                prepend_args = function(self, ctx)
+                    -- Respect buffer indentation settings detected by guess-indent
+                    local indent_type = vim.bo[ctx.buf].expandtab and 'Spaces' or 'Tabs'
+                    local indent_width = vim.bo[ctx.buf].shiftwidth
+                    return {
+                        '--indent-type',
+                        indent_type,
+                        '--indent-width',
+                        tostring(indent_width),
+                    }
+                end,
+            },
+            rustfmt = {
+                prepend_args = function(self, ctx)
+                    -- Respect buffer indentation settings detected by guess-indent
+                    local args = {}
+                    if vim.bo[ctx.buf].expandtab then
+                        table.insert(args, '--config')
+                        table.insert(args, 'hard_tabs=false')
+                        table.insert(args, '--config')
+                        table.insert(args, 'tab_spaces=' .. tostring(vim.bo[ctx.buf].shiftwidth))
+                    else
+                        table.insert(args, '--config')
+                        table.insert(args, 'hard_tabs=true')
+                    end
+                    return args
+                end,
+            },
         },
-      },
     },
 }
